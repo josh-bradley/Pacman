cdd2bbbfdf6ff8133b75c40ecc50c1260073070d
Matthew Davis <github@mdavis.xyz>
2019-03-25 06:01:25 +0000

    score text increments

--- XR-Starter/Assets/_game/Scripts/PointCounter.cs
+++ XR-Starter/Assets/_game/Scripts/PointCounter.cs
@@ -6,10 +6,15 @@ public class PointCounter : MonoBehaviour
 {
 
     private int score = 0;
-
+    
     public void Increment()
     {
         score++;
+
+        UnityEngine.UI.Text scoreText = GameObject.FindGameObjectWithTag("scoreText").GetComponent<UnityEngine.UI.Text>();
+
+        scoreText.text = $"Score = {score}";
+
         Debug.Log("Score incremented");
 
 
