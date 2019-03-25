5eef4c1424febbefeed17327b5050221d32a91a2
Matthew Davis <github@mdavis.xyz>
2019-03-25 04:15:44 +0000

    colour change based on distance for sphere

---
+++ XR-Starter/Assets/_game/Scripts/ProximityColourer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityColourer : MonoBehaviour
+{
+
+    public Color farColour = Color.yellow;
+    public Color closeColour = Color.green;
+    public MeshRenderer mesh;
+    float t = 0;
+    public float threshold = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //t += Time.deltaTime;
+        //t = t % 1;
+        //mesh.material.color = Color.Lerp(farColour, closeColour, t);
+
+
+        ;
+
+        float dist = Vector3.Distance(mesh.transform.position, Camera.main.transform.position);
+        
+
+        if (dist < threshold)
+        {
+            mesh.material.color = closeColour;
+        }
+        else
+        {
+            mesh.material.color = farColour;
+        }
+    }
+}
