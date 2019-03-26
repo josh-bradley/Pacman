ca987eb8e791283a6e78615e24d511d85b3696ae
tabbrd <josh.bradley@rwwa.com.au>
2019-03-26 13:58:55 +1100

    Added end game sound

---
+++ XR-Starter/Assets/_game/Scripts/CatchPacman.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CatchPacman : MonoBehaviour
+{
+    public AudioClip gameOver;
+    private AudioSource gameAudio;
+    private bool GameOver = false;
+
+    private void Awake()
+    {
+        gameAudio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!GameOver)
+        {
+            gameAudio.clip = gameOver;
+            gameAudio.Play();
+            GameOver = true;
+        }
+    }
+}
